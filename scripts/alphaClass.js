17be6ea345235e1b2a125904703a62e518861b41
captainboogey <anik35-700@diu.edu.bd>
2024-10-25 03:50:33 +0600

    keyboard function added

--- scripts/alphaClass.js
+++ scripts/alphaClass.js
@@ -7,8 +7,57 @@
 // playGroundSection.classList.remove('hidden');
 
 // }
+function handleKeyboardKeyUpEvent(event) {
+  const playerPressed = event.key;
+  console.log("player pressed", playerPressed);
+  const currentAlphabet = document.getElementById("current-alphabet");
+  // console.log(currentAlphabet.innerText);
+  const currentAlphabet2 = currentAlphabet.innerText;
+  const expectedAlphabet = currentAlphabet2.toLowerCase();
+  console.log(playerPressed, expectedAlphabet);
+  if (playerPressed === expectedAlphabet) {
+    console.log("you get a point");
+    const currentScoreElement =document.getElementById('current-score')
+    const currentScoreText = currentScoreElement.innerText;
+    console.log(currentScoreText);
+    const currentScore =parseInt(currentScoreText);
+    const newScore = currentScore+1;
+    currentScoreElement.innerText =newScore;
 
-function play(){
-    hideElementById('home-screen');
-    showElementById('play-ground');
+
+
+
+
+
+    removeBackgroundColorById(expectedAlphabet);
+    
+    continueGame();
+    
+  } else {
+    console.log("you missed you lost a point and a life");
+    const currentLifeElement = document.getElementById('current-life');
+    const currentLifeText = currentLifeElement.innerText;
+    const currentlife = parseInt(currentLifeText);
+    const newLife = currentlife-1;
+    currentLifeElement.innerText = newLife;
+    
+  }
+}
+
+document.addEventListener("keyup", handleKeyboardKeyUpEvent);
+
+function continueGame() {
+  const alphabet = getARandomAlphabet();
+
+  //    set randomly genarate alphabet in the screen
+  const currentAlphabet = document.getElementById("current-alphabet");
+  currentAlphabet.innerText = alphabet;
+
+  setHightlightColorByid(alphabet);
+}
+
+function play() {
+  hideElementById("home-screen");
+  showElementById("play-ground");
+  continueGame();
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -6,3 +6,26 @@ function showElementById(elementId){
     const showElement =document.getElementById(elementId);
     showElement.classList.remove('hidden');
 }
+function setHightlightColorByid(elementId){
+      const element =document.getElementById(elementId);
+      element.classList.add('bg-orange-400')
+
+}
+function removeBackgroundColorById(elementId){
+         const element =document.getElementById(elementId);
+         element.classList.remove('bg-orange-400')
+}
+
+function getARandomAlphabet(){
+    const alphabetString ='abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+    console.log(alphabets)
+    const randomNumber =Math.random()*25;
+    const index =Math.round(randomNumber);
+    
+    const alphabet = alphabets[index];
+    
+    
+    return alphabet;
+    
+}
